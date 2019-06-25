d40e0d41b3ba53eb8accbbbfe2f37ed01848fa68
José Manuel <joseliza@gmail.com>
2019-06-25 09:34:08 +0200

    Aprendiendo herencia

--- Contacto.py
+++ Contacto.py
@@ -1,7 +1,17 @@
 class Contacto:
+    '''Clase contacto'''
     todos_contactos = []
 
     def __init__(self, nombre, email):
         self.nombre = nombre
         self.email = email
         Contacto.todos_contactos.append(self)
+
+
+class Vendedor(Contacto):
+    '''Clase vendedor que hereda de la clase contacto'''
+
+
+    def pedido(self, pedido):
+        print("En una aplicación completa enviaría el pedido "
+        "{} pedido a {}".format(pedido, self.nombre))
